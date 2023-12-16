b481c00c28bf3d9ab1978f830807260286dfcb96
lpopivnyak <stadnykroman2010@gmail.com>
2023-12-16 19:38:29 +0200

    Memory_card 6

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import random
 
 from PyQt5.QtWidgets import *
 
+import roma
+
 app = QApplication([])
 
 window = QWidget()
 window.resize(700,500)
 
 menu_btn = QPushButton("Меню")
+next_quest_btn = QPushButton("Наступне запитаня")
 grou_box = QGroupBox("Варіанти відповідей")
 spin_box = QSpinBox()
 qesting_lbl = QLabel("Яблуко")
@@ -15,6 +18,8 @@ answer1_btn = QRadioButton("Bu4rlding")
 answer2_btn = QRadioButton("home")
 answer3_btn = QRadioButton("applicate")
 answer4_btn = QRadioButton("apple")
+resulat_lbl = QLabel("Результат")
+resulat_lbl.hide()
 
 answer = [answer1_btn,answer2_btn,answer3_btn,answer4_btn]
 random.shuffle(answer)
@@ -24,24 +29,53 @@ vidpovist_btn = QPushButton("Відповісти")
 mine_line = QVBoxLayout()
 
 h1 = QHBoxLayout()
-h2 = QHBoxLayout()
 h1.addWidget(menu_btn)
 h1.addStretch(1)
 mine_line.addLayout(h1)
-mine_line.addLayout(h2)
 mine_line.addWidget(qesting_lbl)
 
 group_line = QVBoxLayout()
-group_line1 = QVBoxLayout()
+
 group_line.addWidget(answer[0])
 group_line.addWidget(answer[1])
-group_line1.addWidget(answer[2])
-group_line1.addWidget(answer[3])
+group_line.addWidget(answer[2])
+group_line.addWidget(answer[3])
+group_line.addWidget(resulat_lbl)
 grou_box.setLayout(group_line)
 
-mine_line.addWidget(grou_box)
 
+mine_line.addWidget(grou_box)
+mine_line.addWidget(vidpovist_btn)
+mine_line.addWidget(next_quest_btn)
 window.setLayout(mine_line)
+
+def set_questons():
+    num = roma.questons_number
+    qesting_lbl.setText(roma.questons[num]["Запитаня"])
+    answer[0].setText(roma.questons[num]["Правильна відповідь"])
+    answer[1].setText(roma.questons[num]["Неправилна відповідь 1"])
+    answer[2].setText(roma.questons[num]["Неправилна відповідь 2"])
+    answer[3].setText(roma.questons[num]["Неправилна відповідь 3"])
+set_questons()
+
+def answer_click():
+    if answer[0].isChecked():
+        resulat_lbl.setText("Правильно")
+    else:
+        resulat_lbl.setText("Неправильно")
+    resulat_lbl.show()
+    answer[0].hide()
+    answer[1].hide()
+    answer[2].hide()
+    answer[3].hide()
+
+def next_quest_func():
+    roma.questons_number+=1
+    set_questons()
+
+
+vidpovist_btn.clicked.connect(answer_click)
+next_quest_btn.clicked.connect(next_quest_func)
 window.show()
 app.exec()
 
