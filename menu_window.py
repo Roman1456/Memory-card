7fc3eb38329120c96e64aebd113a3ad0f9ee1f05
Roman1456 <151393377+Roman1456@users.noreply.github.com>
2023-12-24 16:17:38 +0300

    Memo_card 9

--- menu_window.py
+++ menu_window.py
@@ -7,8 +7,11 @@ def menu_window():
     window = QDialog()
     qest_lbl = QLabel("Ведіть запитаня")
     right_ans_lbl = QLabel("Ведіть правильну відповідь")
+    right_ans_lb
     qest_edit = QLineEdit()
-    right_ans_edit = QLineEdit()
+    qest_edit1 = QLineEdit()
+    qest_edit2 = QLineEdit()
+    qest_edit3 = QLineEdit()
     add_quest_btn = QPushButton("Добавити запитаня")
     main_line = QVBoxLayout()
     h1 = QHBoxLayout()
@@ -16,10 +19,20 @@ def menu_window():
     h1.addWidget(qest_edit)
     main_line.addLayout(h1)
 
+    h2 = QHBoxLayout()
+    h2.addWidget(right_ans_lbl)
+    h2.addWidget(qest_edit1)
+    main_line.addLayout(h2)
+
+    h3 = QHBoxLayout()
+    h3.addWidget()
+
+
+
     def add_quest_func():
         a ={
             "Запитаня": qest_edit.text(),
-            "Правильна відповідь": "",
+            "Правильна відповідь": qest_edit1.text(),
             "Неправилна відповідь 1": "",
             "Неправилна відповідь 2": "",
             "Неправилна відповідь 3": "",
